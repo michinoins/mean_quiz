16969304e6e1d5a2875ed1b0bb91ebdadb559984
michinoins <m1i1k2i1ya@gmail.com>
2022-02-28 10:04:25 +0900

    fix: quiz to stateful

--- lib/quiz_list/quiz_page.dart
+++ lib/quiz_list/quiz_page.dart
@@ -1,19 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/quiz_list/quiz_list_model.dart';
-import 'package:flutter_app/result.dart';
+import 'package:flutter_app/entity/quiz.dart';
 
-import '../entity/quiz.dart';
+import '../result.dart';
 
 class Question extends StatefulWidget {
   @override
   _QuestionState createState() => _QuestionState();
 }
 
-
 class _QuestionState extends State<Question> {
-  QuizListModel quiz = QuizListModel();
-  Future<List<Quiz>> _getQuiz() async{
-    return await quiz.fetchBookList();
+  List<Quiz> quiz = [];
+
+  // todo future builder で何か切り出すが正かな...?
+  setQuiz() async {
+    // 検索結果は、一旦はFuture型で取得
+    Future<QuerySnapshot<Map<String, dynamic>>> snapshot =
+        FirebaseFirestore.instance.collection("quiz").get();
+
+    // ドキュメントを取得
+    await snapshot.then((value) {
+      setState(() {
+        quiz = value.docs.map((doc) => Quiz.fromDocument(doc)).toList();
+      });
+    });
+    print("hoge" + quiz.elementAt(0).sentence);
   }
 
   static const List<String> questionSentences = [
@@ -33,7 +44,7 @@ class _QuestionState extends State<Question> {
 
   @override
   Widget build(BuildContext context) {
-
+    setQuiz();
     return Scaffold(
         appBar: AppBar(
           title: Text("問題ページ"),
@@ -54,100 +65,96 @@ class _QuestionState extends State<Question> {
           Container(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                quiz.,
+                quiz.elementAt(questionNumber).sentence,
                 style: TextStyle(fontSize: 20),
               )),
           Expanded(
               child: Padding(
                   padding: const EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 50.0),
-                  child: FutureBuilder(
-                    future:_getQuiz(),
-                    builder: (BuildContext context, AsyncSnapshot<List<Quiz>> snapshot).
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SizedBox(
-                              width: double.infinity,
-                              height: 50,
-                              child: ElevatedButton(
-                                child: Text("まる"),
-                                onPressed: () {
-                                  bool answer = true;
-                                  // quiz.elementAt(questionNumber).answer;
-                                  if (answer == true) {
-                                    // 正解の処理
-                                    numOfCorrectAnswers++;
-                                    if (questionNumber + 1 < 3) {
-                                      setState(() {
-                                        questionNumber++;
-                                      });
-                                    } else {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Result(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                              child: Text("まる"),
+                              onPressed: () {
+                                bool answer =
+                                    quiz.elementAt(questionNumber).answer;
+                                if (answer == true) {
+                                  // 正解の処理
+                                  numOfCorrectAnswers++;
+                                  if (questionNumber + 1 < quiz.length) {
+                                    setState(() {
+                                      questionNumber++;
+                                    });
+                                  } else {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Result(
+                                                numberOfCorrectAnswer:
+                                                    numOfCorrectAnswers)));
+                                  }
+                                } else {
+                                  if (questionNumber + 1 < quiz.length) {
+                                    setState(() {
+                                      questionNumber++;
+                                    });
+                                  } else {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Result(
                                                   numberOfCorrectAnswer:
-                                                      numOfCorrectAnswers)));
-                                    }
-                                  } else {
-                                    if (questionNumber + 1 < 3) {
-                                      setState(() {
-                                        questionNumber++;
-                                      });
-                                    } else {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Result(
-                                                    numberOfCorrectAnswer:
-                                                        numOfCorrectAnswers,
-                                                  )));
-                                    }
+                                                      numOfCorrectAnswers,
+                                                )));
                                   }
-                                },
-                              )),
-                          SizedBox(
-                              width: double.infinity,
-                              height: 50,
-                              child: ElevatedButton(
-                                child: const Text("ばつ"),
-                                onPressed: () {
-                                  bool answer = answers[questionNumber];
-                                  if (answer == false) {
-                                    // 正解の処理
-                                    numOfCorrectAnswers++;
-                                    if (questionNumber + 1 < 3) {
-                                      setState(() {
-                                        questionNumber++;
-                                      });
-                                    } else {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Result(
-                                                    numberOfCorrectAnswer:
-                                                        numOfCorrectAnswers,
-                                                  )));
-                                    }
+                                }
+                              },
+                            )),
+                        SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                              child: const Text("ばつ"),
+                              onPressed: () {
+                                bool answer = answers[questionNumber];
+                                if (answer == false) {
+                                  // 正解の処理
+                                  numOfCorrectAnswers++;
+                                  if (questionNumber + 1 < quiz.length) {
+                                    setState(() {
+                                      questionNumber++;
+                                    });
                                   } else {
-                                    if (questionNumber + 1 < 3) {
-                                      setState(() {
-                                        questionNumber++;
-                                      });
-                                    } else {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => Result(
-                                                    numberOfCorrectAnswer:
-                                                        numOfCorrectAnswers,
-                                                  )));
-                                    }
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Result(
+                                                  numberOfCorrectAnswer:
+                                                      numOfCorrectAnswers,
+                                                )));
                                   }
-                                },
-                              ))
-                        ]),
-                  )))
+                                } else {
+                                  if (questionNumber + 1 < quiz.length) {
+                                    setState(() {
+                                      questionNumber++;
+                                    });
+                                  } else {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Result(
+                                                  numberOfCorrectAnswer:
+                                                      numOfCorrectAnswers,
+                                                )));
+                                  }
+                                }
+                              },
+                            ))
+                      ])))
         ])));
   }
 }
